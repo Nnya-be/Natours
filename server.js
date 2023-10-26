7f796d0b27e92b6dde91bd05a08f1643b78a3507
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-26 21:02:24 +0000

    changes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env',
 });
 
+const DB = process.env.DATABASE.replace('<password>', process.env.DB_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connected');
+  });
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
