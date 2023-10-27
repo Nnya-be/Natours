bf31dc2e88666b82fd616d3adcd1a0ab19afd23f
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-27 19:43:35 +0000

    Created: Database connection file with configs

---
+++ connectDB.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+ 
+export default function connectDB() {
+  const url = "mongodb://127.0.0.1/blog_db";
+ 
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    console.log(`Database connected: ${url}`);
+  });
+ 
+  dbConnection.on("error", (err) => {
+    console.error(`connection error: ${err}`);
+  });
+  return;
+}
