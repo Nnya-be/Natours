7d234c8627a8d7f999568e1bda6fce29a16201e7
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-27 22:59:29 +0000

    Modified: Tour Controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,5 @@
 const fs = require('fs');
-const tourModel = require('../models/tourModel');
-
-exports.checkBody = (req, res, next) => {
-  // const body = req.body.name;
-  // const price = req.body.price;
-  // if (!body || !price) {
-  //   return res.status(400).json({
-  //     status: 'failed',
-  //     message: 'Trying to create a tour without a body or price',
-  //   });
-  // }
-
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -36,23 +23,21 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  // const newID = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newID }, req.body);
-
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   },
-  // );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
