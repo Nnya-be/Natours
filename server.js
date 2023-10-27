9ccb535580eb7ddb22c088be3349a7e9a4097bf3
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-27 20:22:06 +0000

    Restored: previous version

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ mongoose.connect(DB, {
   useCreateIndex: true,
 });
 
-const tourSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
 
 // .then((con) => {
 //   console.log(con.connections);
