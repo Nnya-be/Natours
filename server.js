40f856ca95d0fd50b31cc571f7147fd011fc73b4
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-28 18:40:10 +0000

    Changes: Server receives all requests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({
   path: './config.env',
 });
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DB_PASSWORD);
