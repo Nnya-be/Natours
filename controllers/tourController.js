548dfe10d6a16dbbf75a519479b329dfcc209bad
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-28 19:17:53 +0000

    Added: pagination implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,6 +40,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //pagination
+    // console.log(req.query);
+    const page = req.query.page * 1 || 1;
+
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //Sending the query
     const tours = await query;
     res.status(200).json({
