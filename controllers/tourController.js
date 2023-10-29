315e54c4e048aecb2b788e25734368f0412bc021
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-29 18:42:25 +0000

    Added: functionality for API refactoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const Tour = require('../models/tourModel');
 const { query } = require('express');
 
+exports.aliasTopTours =  (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   // const tours = await Tour.find()
   //   .where('duration')
