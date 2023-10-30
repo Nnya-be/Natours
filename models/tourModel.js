a1144cb159cae0699d944400091ab9f4dbc839c0
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-10-30 10:56:59 +0000

    Created: all the functionalities

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal to 40 characters'],
       minlength: [10, 'A tour name must have more or equal to 10 characters'],
+      // validate: [validator.isAlpha,"Tour name must only contain characters"],
     },
     slug: String,
     duration: {
@@ -28,7 +30,15 @@ const tourSchema = new mongoose.Schema(
       max: [5, 'A rating must not exceede 5.0'],
     },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price should be below the regular price',
+      },
+    },
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
