dad0ee6f4f9c9c8f2939bc3187863e84462e70f9
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-12-03 21:37:34 +0000

    Next update

--- models/userModel.js
+++ models/userModel.js
@@ -49,6 +49,12 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.checkPassword = async function (
   doc_password,
   user_password,
