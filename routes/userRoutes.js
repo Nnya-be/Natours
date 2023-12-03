b2b401d96ef70d1d54fd420f5b44195f4d87bff2
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-12-03 21:37:53 +0000

    Still Modified

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,11 @@ router.post('/signup', authController.signUp);
 router.post('/login', authController.logIn);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword,
+);
 router
   .route('/')
   .get(userController.getAllUsers)
