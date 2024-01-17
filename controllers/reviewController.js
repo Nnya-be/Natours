830be6a62256578a36d2f7571ab3c7e80c10b170
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-17 17:49:24 +0000

    Created: a review controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,45 @@
+const catchAsync = require('../utils/catchAsync');
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+
+  next();
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+    
+        //setting info for the nested routes.
+    if (!req.body.tour) req.body.tour = req.params.tourId;
+    if(!req.body.user) req.body.user = req.user.id;
+    const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError('No review found', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
