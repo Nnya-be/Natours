9e08fa98bbe11971336c8fc698d75dba19047362
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-29 23:32:45 +0000

    Created: Delete handler for one review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,13 @@
 const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
+  let fileter = {};
+  if (req.params.tourId) {
+    filter = { tour: req.params.tourId };
+  }
   const reviews = await Review.find();
 
   res.status(200).json({
@@ -16,11 +22,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-    
-        //setting info for the nested routes.
-    if (!req.body.tour) req.body.tour = req.params.tourId;
-    if(!req.body.user) req.body.user = req.user.id;
-    const newReview = await Review.create(req.body);
+  //setting info for the nested routes.
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  const newReview = await Review.create(req.body);
 
   res.status(201).json({
     status: 'success',
@@ -43,3 +48,5 @@ exports.getReview = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
